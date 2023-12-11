ad347c693444a237236810fa489e096872cdfe0b
Fabelixio <felixsewellknight@gmail.com>
2023-12-11 17:00:23 +0000

    add additional article card styling

--- src/components/cards/ArticleCard.jsx
+++ src/components/cards/ArticleCard.jsx
@@ -5,10 +5,10 @@ const ArticleCard = ({article}) => {
         <li>
             <div className="articlecard">
                 <h2 className="article-card-header">{article.title}</h2>
-                <p>{article.author}</p>
+                <p className="article-card-contents">{article.author}</p>
                 <img src={article.article_img_url} alt='article cover image' />
-                <p> Date Posted: {article.created_at}</p>
-                <p>Comments: {article.comment_count}</p>
+                <p className="article-card-contents"> Date Posted: {article.created_at}</p>
+                <p className="article-card-contents">Comments: {article.comment_count}</p>
                 <button className="article-button">Go to Article</button>
             </div>
         </li>
