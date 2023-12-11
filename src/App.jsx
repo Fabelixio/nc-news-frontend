f05b2bef6eac568d81c59aef2d7c06523a9de2e9
Fabelixio <felixsewellknight@gmail.com>
2023-12-11 15:33:14 +0000

    create basic header

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import './App.css'
 import { Routes, Route } from 'react-router-dom'
 import ArticleList from './components/ArticleList'
+import Header from './components/Header'
 
 const App = () => {
   return (
+    <>
+    <Header />
     <Routes>
       <Route path='/articles' element={<ArticleList />}/>
     </Routes>
+    </>
   )
 }
 export default App
