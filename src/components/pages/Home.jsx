04e9119222077a20639db6cb1a5b3b877dac91e3
Fabelixio <felixsewellknight@gmail.com>
2024-01-31 16:18:02 +0000

    update homepage

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -4,6 +4,7 @@ const Home = () => {
         <>
         <h2 className="text-center font-young underline">Welcome to NC news!</h2>
         <p className="text-center font-young">This project aims to simulate a real world website. It utilises a functioning database for API calls, which are then displayed on the website. the full project was built over the course of 2 weeks and is my first attempt at building a functioning web service. Enjoy!</p>
+        <p className="text-center font-young">The sites content may take a few moments to load upon initial viewing.</p>
         </>
     )
 }
